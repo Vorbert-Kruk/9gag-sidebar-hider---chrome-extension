398368d2064e8bb4b99649160602dcc33350190a
Vorbert Kruk <pryrokopter@gmail.com>
2020-02-14 22:43:03 +0100

    Wrappnięto wszystko w funkcję, by deklaracje zmiennych się nie powtarzay

--- hidingFunctionality.js
+++ hidingFunctionality.js
@@ -1,24 +1,26 @@
-let sideBarHasBeenDeleted = false;
-
-const removeElement = element => {
-  element &&
-    element.parentElement &&
-    element.parentElement.removeChild(element);
-  sideBarHasBeenDeleted = true;
-};
-
-const removeSideBar = () => {
-  const sideBar = document.querySelector('#sidebar-content');
-  removeElement(sideBar);
-};
-
 (() => {
-  removeSideBar();
+  let sideBarHasBeenDeleted = false;
 
-  if (!sideBarHasBeenDeleted) {
-    const removingInterval = setInterval(() => {
-      sideBarHasBeenDeleted && clearInterval(removingInterval);
-      removeElement(sideBar);
-    }, 1000);
-  }
+  const removeElement = element => {
+    element &&
+      element.parentElement &&
+      element.parentElement.removeChild(element);
+    sideBarHasBeenDeleted = true;
+  };
+
+  const removeSideBar = () => {
+    const sideBar = document.querySelector('#sidebar-content');
+    removeElement(sideBar);
+  };
+
+  (() => {
+    removeSideBar();
+
+    if (!sideBarHasBeenDeleted) {
+      const removingInterval = setInterval(() => {
+        sideBarHasBeenDeleted && clearInterval(removingInterval);
+        removeElement(sideBar);
+      }, 1000);
+    }
+  })();
 })();
