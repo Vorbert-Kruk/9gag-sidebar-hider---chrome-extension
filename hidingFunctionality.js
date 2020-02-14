ffe49cf15fa1a7112ce3bdfb2def08e6f3a7d57f
Vorbert Kruk <pryrokopter@gmail.com>
2020-02-14 20:20:25 +0100

    Dodano zapasowe sprawdzanko interwałem, czy sidebar nadal istnieje

--- hidingFunctionality.js
+++ hidingFunctionality.js
@@ -1,6 +1,24 @@
-const removeElement = element => element.parentElement.removeChild(element);
+let sideBarHasBeenDeleted = false;
+
+const removeElement = element => {
+  element &&
+    element.parentElement &&
+    element.parentElement.removeChild(element);
+  sideBarHasBeenDeleted = true;
+};
+
+const removeSideBar = () => {
+  const sideBar = document.querySelector('#sidebar-content');
+  removeElement(sideBar);
+};
 
 (() => {
-  const sideBar = document.querySelector('#sidebar-content');
-  sideBar && removeElement(sideBar);
+  removeSideBar();
+
+  if (!sideBarHasBeenDeleted) {
+    const removingInterval = setInterval(() => {
+      sideBarHasBeenDeleted && clearInterval(removingInterval);
+      removeElement(sideBar);
+    }, 1000);
+  }
 })();
